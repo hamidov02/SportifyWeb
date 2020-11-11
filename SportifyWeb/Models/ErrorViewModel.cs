21bdb02777d4dea08cabec4cd8e6ebf5a6f901ae
Akbar Hamidov <akbar.hamidov@edu.bme.hu>
2020-11-11 21:15:12 +0100

    Repository created..

---
+++ SportifyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportifyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
