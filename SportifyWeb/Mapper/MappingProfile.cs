e7fb4b51f8045ff0b94719d8cdd385304d632155
--global <--global>
2020-12-06 11:35:07 +0100

    Mapper was added

---
+++ SportifyWeb/Mapper/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportifyWeb;
+using SportifyWeb.Resources;
+using SportifyWeb.Models;
+
+namespace SportifyWeb.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResource>();
+            CreateMap<Stadium, StadiumResource>();
+            CreateMap<Adress, AdressResource>();
+            CreateMap<StadiumActivity, StadiumActivityResource>();
+            CreateMap<CreditCard, CreditCardResource>();
+            CreateMap<Payment, PaymentResource>();
+            CreateMap<Order, OrderResource>();
+        }
+    }
+}
